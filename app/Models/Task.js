6252719a8d1d28e6980dbb9662908349313b16f2
Igor Souza <igorsouza.dev@gmail.com>
2019-12-27 15:36:40 -0300

    Added models relationships

--- app/Models/Task.js
+++ app/Models/Task.js
@@ -1,9 +1,20 @@
-'use strict'
+'use strict';
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
 class Task extends Model {
+  user () {
+    return this.belongsTo('App/Models/User')
+  }
+
+  project () {
+    return this.belongsTo('App/Models/Project')
+  }
+
+  file () {
+    return this.belongsTo('App/Models/File')
+  }
 }
 
 module.exports = Task
